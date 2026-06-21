b3b84f412b2259b15fc1c138664ad2ae33ed94d6
kingtophk <woyao3721@gmail.com>
2026-06-22 00:49:57 +0800

    212

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-
-let zh=false;
-document.getElementById('langBtn').onclick=function(){
-zh=!zh;
-document.querySelectorAll('[data-en]').forEach(el=>{
-el.textContent=zh?el.dataset.zh:el.dataset.en;
-});
-}
+console.log('KingTop');
