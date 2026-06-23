58f358aed79dde900758bf5e5090c3bf1608fb1f
kingtophk <woyao3721@gmail.com>
2026-06-23 18:51:02 +0800

    123

--- script.js
+++
@@ -1,9 +0,0 @@
-
-let zh=false;
-document.getElementById('langBtn').onclick=()=>{
- zh=!zh;
- document.querySelectorAll('[data-en]').forEach(el=>{
-   el.textContent=zh?el.dataset.zh:el.dataset.en;
- });
- document.getElementById('langBtn').textContent=zh?'EN':'繁中';
-};
