0fd53d4463f81f4f3ead395e0f8d77fe5d86662c
kingtophk <woyao3721@gmail.com>
2026-06-23 18:55:59 +0800

    11111

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 const toggle = document.getElementById("langToggle");
+
 toggle.addEventListener("click", () => {
   const el = document.querySelectorAll("[data-en]");
   const zh = toggle.textContent.includes("中");
+
   el.forEach(e => {
     e.textContent = zh ? e.dataset.en : e.dataset.zh;
   });
+
   toggle.textContent = zh ? "EN / 中" : "中 / EN";
 });
